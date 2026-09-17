7c87643faff16081f3eacbddb464cde85a3e15c3
iamdp09 <darshanpathak8899@gmail.com>
2026-09-17 17:25:33 +0530

    setting up custom exceptions

--- demo/src/main/java/Repora/backend/demo/exceptions/UnauthorizedException.java
+++ demo/src/main/java/Repora/backend/demo/exceptions/UnauthorizedException.java
@@ -1,4 +1,11 @@
 package Repora.backend.demo.exceptions;
 
-public class UnauthorizedExcption {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
 }
